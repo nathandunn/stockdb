a1f9064d4589d9846375deca3a9242c7a01ad829
Nathan Dunn <ndunn@uoregon.edu>
2013-05-21 12:39:10 -0700

    got stuff to work

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -33,7 +33,10 @@ grails.mime.types = [
 //grails.urlmapping.cache.maxsize = 1000
 
 // What URL patterns should be processed by the resources plugin
-grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
+grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*','/gwt/*']
+//grails.resources.debug = true
+//grails.resources.modules = {}
+grails.resources.adhoc.excludes = ['**/gwt/**']
 
 // The default codec used to encode data with ${}
 grails.views.default.codec = "none" // none, html, base64
