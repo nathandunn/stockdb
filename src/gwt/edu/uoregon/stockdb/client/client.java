4c9b94959cfe3ba7fd6bc235976c78f1b88de899
Nathan Dunn <ndunn@me.com>
2013-05-20 21:41:37 -0700

    the gwt code is doing a round-trip

--- src/gwt/edu/uoregon/stockdb/client/client.java
+++ src/gwt/edu/uoregon/stockdb/client/client.java
@@ -2,9 +2,6 @@ package edu.uoregon.stockdb.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
@@ -18,31 +15,42 @@ public class client implements EntryPoint {
     private static QuickEntryServiceAsync myService = GWT.create(QuickEntryService.class);
 
     private Button button = new Button("click me!");
+    private Label label = new Label("no call made yet");
+
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
 
-        RootPanel.get().add(new Label("its kind of working"));
+        RootPanel.get().add(label);
 
-
-        RootPanel.get().add(button);
-        button.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-//                myService.doit(new MyAsyncCallback(label));
-//                Window.alert("clicked");
-                myService.doit(new AsyncCallback() {
-                    public void onFailure(Throwable caught) {
-                        Window.alert("fail:\n"+caught);
-                    }
-
-                    public void onSuccess(Object result) {
-                        Window.alert("suceed:\n"+result);
-                    }
-                });
+        myService.doit(new AsyncCallback() {
+            public void onFailure(Throwable caught) {
+                label.setText("failed - " + caught.getMessage());
             }
-        }
-        );
+
+            public void onSuccess(Object result) {
+                label.setText("succeed - " + result);
+            }
+        });
+
+
+//        RootPanel.get().add(button);
+//        button.addClickHandler(new ClickHandler() {
+//            public void onClick(ClickEvent event) {
+////                myService.doit(new MyAsyncCallback(label));
+////                Window.alert("clicked");
+//                myService.doit(new AsyncCallback() {
+//                    public void onFailure(Throwable caught) {
+//                        label.setText("failed - "+caught.getMessage());
+//                    }
+//
+//                    public void onSuccess(Object result) {
+//                        label.setText("succeed - "+result);
+//                    }
+//                });
+//            }
+//        } );
 
 
     }
