f6de5520f2a80a6e808c6d85e32cd1c22b066b0b
Nathan Dunn <ndunn@uoregon.edu>
2013-06-03 12:32:08 -0700

    added the UI basics

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,6 +30,10 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+
+        // for mail plugin
+        mavenRepo "http://download.java.net/maven/2/"
+
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
@@ -64,5 +68,11 @@ grails.project.dependency.resolution = {
 
 
         // plugins.shiro=1.1.4
+        compile ":shiro:1.1.4"
+        compile ":shiro-ui:1.2.0-SNAPSHOT"
+        compile ":mail:1.0.1"
+
+
+
     }
 }
