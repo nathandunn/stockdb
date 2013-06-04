73162bce1a657f0e752c1130033b8da5be7bce8b
ndunn <ndunn@cas.uoregon.edu>
2013-06-04 12:49:14 -0700

    updated

--- update_staging.sh
+++ update_staging.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #hg pull -u 
 
+grails clean
 grails war 
-sudo cp target/stockdb-0.1.war /var/lib/tomcat7/webapps/stockdb.war
+sudo cp target/metagenomicsdb-0.1.war /var/lib/tomcat7/webapps/metagenomicsdb.war
 
 
