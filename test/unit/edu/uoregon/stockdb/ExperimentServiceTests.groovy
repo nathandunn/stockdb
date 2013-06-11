65526f6f6c1a45e61f966193bef9d9fd0f4e02c4
Nathan Dunn <ndunn@uoregon.edu>
2013-06-11 15:05:35 -0700

    fixed experiment  service

--- test/unit/edu/uoregon/stockdb/ExperimentServiceTests.groovy
+++ test/unit/edu/uoregon/stockdb/ExperimentServiceTests.groovy
@@ -1,17 +1,57 @@
 package edu.uoregon.stockdb
 
-
-
-import grails.test.mixin.*
-import org.junit.*
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(ExperimentService)
+@Mock([Experiment, Researcher, MeasuredValue,Category,Strain])
 class ExperimentServiceTests {
 
+    Researcher injectSecurityUser() {
+        Researcher secUser = Researcher.get(1) ?: new Researcher(
+                username: RandomStringGenerator.getRandomEmail()
+                , passwordHash: "secret"
+                , firstName: "George"
+                , lastName: "TheMonkey"
+        ).save(failOnError: true, flush: true)
+        return secUser
+    }
+
     void testSomething() {
-        fail "Implement me"
+        Researcher researcher = injectSecurityUser()
+        Experiment experiment = new Experiment(
+                name: "Motility Experiment"
+                , whenPerformed: new Date()
+                , note: "A note about a motility experiment"
+                , researcher: researcher
+        ).save(failOnError: true)
+
+        Category category = new Category(
+                name: "Motility Category"
+        )
+        .save()
+
+        Strain strain = new Strain()
+
+        MeasuredValue measuredValue = new MeasuredValue(
+                category: category
+                , strain: strain
+                , experiment: experiment
+                , value: "123"
+        ).save(failOnError: true,flush: true)
+
+        category.addToMeasuredValues(measuredValue)
+
+
+        Map<Category, List<MeasuredValue>> map = service.createValuesMap(experiment)
+        assert 1==map.size()
+        println "${map.size()}"
+        Category c2 = map.keySet().iterator().next()
+        assert c2.name=="Motility Category"
+        MeasuredValue m2 = map.get(category).iterator().next()
+        assert m2.value=="123"
     }
 }
