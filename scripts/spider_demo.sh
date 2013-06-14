335b61b6c41b24dc0d80d01ade107712a1a1d525
ndunn <ndunn@cas.uoregon.edu>
2013-06-14 12:07:15 -0700

    updated spider demo

--- scripts/spider_demo.sh
+++ scripts/spider_demo.sh
@@ -1,2 +1,2 @@
- wget --no-check-certificate --spider --recursive --no-verbose --output-file=wget-staging-log.txt --exclude-directories="/job/download/*" https://casspr-demo1.aciss.uroegon.edu/metagenomicsdb &
+ wget --no-check-certificate --spider --recursive --no-verbose --output-file=wget-staging-log.txt --exclude-directories="/job/download/*" https://casspr-demo1.aciss.uoregon.edu/metagenomicsdb &
 
