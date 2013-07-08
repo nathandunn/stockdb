06c28caea5061b18e29cd02840924cc38543c2ba
eschwarz <eschwarz@localhost>
2013-07-08 15:30:17 -0700

    First draft of custom paginator

--- grails-app/taglib/edu/uoregon/stockdb/CustomPaginateTagLib.groovy
+++ grails-app/taglib/edu/uoregon/stockdb/CustomPaginateTagLib.groovy
@@ -6,9 +6,9 @@ class CustomPaginateTagLib {
 		attrs["max"] = params.max
 		def pag = g.paginate(attrs)
 		if (pag == "" && params.max == 10) return; //if there is no need to paginate, don't display the extra stuff
-		out << pag
+		out << '<div class="pag_select">' << pag << '</div>'
 		
 		def textAttrs = [name: 'max', value: params.max]
-		out << "Show " << g.textField(textAttrs) + " rows"
+		out << '<div class="pag_input">Show ' << g.textField(textAttrs) << " rows</div>"
 	}
 }
