809b2576486b7360491836cda686fc4399ff0a7e
ndunn <ndunn@cas.uoregon.edu>
2013-07-22 14:09:59 -0700

    updated

--- update_staging.sh
+++ update_staging.sh
@@ -4,7 +4,7 @@
 THIS_HOST=`hostname`
 if [ "$THIS_HOST" = ubuntu ]; then 
 	grails clean
-	grails war 
+	grails -Dgrails.env=staging war 
 	sudo service tomcat7 stop
 	sudo rm -rf /var/lib/tomcat7/webapps/metagenomicsdb*
 	sudo cp target/metagenomicsdb-0.1.war /var/lib/tomcat7/webapps/metagenomicsdb.war
