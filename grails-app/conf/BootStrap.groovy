236ba09479872c146d74b685dcbd112b1508c3f1
Nathan Dunn <ndunn@uoregon.edu>
2013-10-24 13:27:30 -0700

    removed db miogration thing

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -17,31 +17,31 @@ class BootStrap {
 //            stubData.importExperiments()
 //        }
 
-        // TODO: run this once
-        if(Population.count()==0){
-            Species zebrafish = Species.findByCommonName("Zebrafish")
-            zebrafish.prefix = "Z"
-            zebrafish.save()
-            Species stickleback = Species.findByCommonName("Stickleback")
-            stickleback.prefix = "SB"
-            zebrafish.save()
-
-            HostFacility oregon = HostFacility.findByName("University of Oregon")
-            oregon.prefix = "OR"
-            oregon.save()
-
-            HostFacility northCarolina = HostFacility.findByName("University of North Carolina")
-            northCarolina.prefix = "NC"
-            northCarolina.save()
-
-            HostFacility washington= HostFacility.findByName("Washington University")
-            washington.prefix = "WU"
-            washington.save()
-
-            stubData.stubPhylogeny()
-            Map<String,String> strainMap = stubData.stubStickleback1()
-            stubData.stubStickleback1Experiments(strainMap)
-        }
+//        // TODO: run this once
+//        if(Population.count()==0){
+//            Species zebrafish = Species.findByCommonName("Zebrafish")
+//            zebrafish.prefix = "Z"
+//            zebrafish.save()
+//            Species stickleback = Species.findByCommonName("Stickleback")
+//            stickleback.prefix = "SB"
+//            zebrafish.save()
+//
+//            HostFacility oregon = HostFacility.findByName("University of Oregon")
+//            oregon.prefix = "OR"
+//            oregon.save()
+//
+//            HostFacility northCarolina = HostFacility.findByName("University of North Carolina")
+//            northCarolina.prefix = "NC"
+//            northCarolina.save()
+//
+//            HostFacility washington= HostFacility.findByName("Washington University")
+//            washington.prefix = "WU"
+//            washington.save()
+//
+//            stubData.stubPhylogeny()
+//            Map<String,String> strainMap = stubData.stubStickleback1()
+//            stubData.stubStickleback1Experiments(strainMap)
+//        }
 
     }
     def destroy = {
